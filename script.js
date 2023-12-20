42cabcba2f310e69539c2843b4eb803521e6999d
PatrIck <81175383+PatrIckOgalesco@users.noreply.github.com>
2023-12-20 10:01:45 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Toggle info display
-const infoToggle = document.querySelector('.info-toggle');
-const infoMLWrapper = document.querySelector('.info-ml-wrapper');
-
-infoToggle.addEventListener('click', function () {
-    infoMLWrapper.classList.toggle('show');
-});
